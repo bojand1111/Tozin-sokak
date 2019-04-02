0d94eb40284c6d04a45404eb3a172d280035c093
Bojan Dejanovic <bojandejanovic994@gmail.com>
2019-04-02 12:24:15 +0200

    All sections are finished and ready for inserting pictures and styles.

---
+++ frontend/javascript/custom.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel();
+});
+var owl = $('.owl-carousel');
+owl.owlCarousel({    
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    responsive: {
+      0: {
+        items: 1,      
+      },
+      600: {
+        items: 3,     
+      },
+      1000: {
+        items: 3,      
+      }
+    }
+});
