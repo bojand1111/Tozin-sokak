fa933eaf1142fbcf717a9e747fdb62d0b64b3af3
Bojan Dejanovic <bojandejanovic994@gmail.com>
2019-04-04 01:01:15 +0200

    Add slide images and we offer svg images.

--- frontend/javascript/custom.js
+++ frontend/javascript/custom.js
@@ -2,25 +2,25 @@ $(document).ready(function () {
   $('.owl-carousel').owlCarousel();
 });
 var owl = $('.owl-carousel');
-owl.owlCarousel({    
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
-    responsive: {
-      0: {
-        items: 1,      
-      },
-      600: {
-        items: 3,     
-      },
-      1000: {
-        items: 3,      
-      }
+owl.owlCarousel({
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1200: {
+      items: 3,
     }
+  }
 });
-$('.btn-click').click(function(){
+$('.btn-click').click(function () {
   $('.btn-click').toggleClass('active');
   $('.title').toggleClass('active');
   $('.dropdown-toggle').toggleClass('active');
